7df111ac877a784e261f82ccff2b36ad727fefd4
Jukka Alavesa <juggeli@gmail.com>
2022-09-30 23:56:24 +0300

    yolo

---
+++ modules/services/smb.nix
@@ -0,0 +1,31 @@
+{ options, config, lib, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.smb;
+in {
+  options.modules.services.smb = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.samba = {
+      enable = true;
+
+      # note: you will need to run `sudo smbpasswd -a <user>` to enable a user
+      securityType = "user";
+
+      extraConfig = ''
+        load printers = no
+        printcap name = /dev/null
+        '';
+
+      # shares should be specified per-host
+    };
+
+    networking.firewall = {
+      allowedTCPPorts = [ 445 139 ];
+      allowedUDPPorts = [ 137 138 ];
+    };
+  };
+}
