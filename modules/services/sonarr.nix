5a7be217a63a40587fc95a003b1836939e6886a7
Jukka Alavesa <juggeli@gmail.com>
2022-11-28 10:24:25 +0200

    add sonarr reverse proxy

--- modules/services/sonarr.nix
+++ modules/services/sonarr.nix
@@ -14,6 +14,23 @@ in
       openFirewall = true;
       dataDir = "/mnt/pool/appdata/sonarr";
     };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."sonarr.jugi.cc" = {
+        forceSSL = true;
+        enableACME = true;
+        basicAuth = { sonarr = "4.Y3iiu6CZxrdgyCaFd349Cy"; };
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8989";
+          proxyWebsockets = true;
+        };
+      };
+    };
   };
 }
 
