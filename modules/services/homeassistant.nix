86f14ae528a8b37bc4e49c557399c8f909c916a5
Jukka Alavesa <juggeli@gmail.com>
2022-11-28 10:24:37 +0200

    hass stuff

--- modules/services/homeassistant.nix
+++ modules/services/homeassistant.nix
@@ -4,6 +4,10 @@ with lib;
 with lib.my;
 let
   cfg = config.modules.services.homeassistant;
+  mkLovelaceModule = name: {
+    url = "/local/${name}.js?${pkgs.nur.repos.mweinelt.hassLovelaceModules."${name}".version}";
+    type = "module";
+  };
 in
 {
   options.modules.services.homeassistant = {
@@ -12,6 +16,10 @@ in
 
   config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.nur.overlay ];
+
+    environment.systemPackages = [
+      pkgs.cloudflared
+    ];
 
     services.home-assistant = {
       enable = true;
@@ -43,29 +51,47 @@ in
         "ipp"
       ];
       config = {
-        # Includes dependencies for a basic setup
-        # https://www.home-assistant.io/integrations/default_config/
         default_config = { };
+        http = {
+          server_host = "::1";
+          trusted_proxies = [ "::1" ];
+          use_x_forwarded_for = true;
+        };
       };
     };
 
-    # Custom Lovelace Modules
     systemd.tmpfiles.rules = [
-      "d /var/lib/hass/www 0755 hass hass"
-      "L /var/lib/hass/www/apexcharts-card.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.apexcharts-card}/apexcharts-card.js"
-      "L /var/lib/hass/www/mushroom.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.mushroom}/mushroom.js"
+      "d /run/hass 0700 nginx nginx"
+      "L+ /run/hass/apexcharts-card.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.apexcharts-card}/apexcharts-card.js"
+      "L+ /run/hass/mushroom.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.mushroom}/mushroom.js"
     ];
 
-    services.home-assistant.lovelaceConfig.resources = [
-      {
-        url = "local/apexcharts-card.js";
-        type = "module";
-      }
-      {
-        url = "local/mushroom.js";
-        type = "module";
-      }
-    ];
+    services.home-assistant.config.lovelace = {
+      resources = [
+        (mkLovelaceModule "apexcharts-card")
+        (mkLovelaceModule "mushroom")
+      ];
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."hass.jugi.cc" = {
+        forceSSL = true;
+        enableACME = true;
+        extraConfig = ''
+          proxy_buffering off;
+        '';
+        locations."/" = {
+          proxyPass = "http://[::1]:8123";
+          proxyWebsockets = true;
+        };
+        locations."/local/" = {
+          alias = "/run/hass/";
+        };
+      };
+    };
+    security.acme.defaults.email = "juggeli@gmail.com";
 
     services.postgresql = {
       enable = true;
@@ -88,7 +114,7 @@ in
     };
 
     services.zigbee2mqtt = {
-      enable = true;
+      enable = false;
       dataDir = "/mnt/pool/appdata/zigbee2mqtt";
       settings = {
         homeassistant = config.services.home-assistant.enable;
@@ -104,6 +130,23 @@ in
 
     networking.firewall = {
       allowedTCPPorts = [ config.services.home-assistant.config.http.server_port 8090 ];
+    };
+
+    users.users.cloudflared = {
+      group = "cloudflared";
+      isSystemUser = true;
+    };
+    users.groups.cloudflared = { };
+
+    systemd.services.cloudflare-tunnel = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" "systemd-resolved.service" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token=eyJhIjoiMTMyMjkxYzEyNmQ1MTBjZDg1N2VkZGMzNjdhMmM3N2EiLCJ0IjoiMmM0NDc1ZTEtNzdlNC00NmJkLWFhYmUtNDJhMjY1Y2VjNWZkIiwicyI6Ik1ETmpNMkptWmpndE5EQmtZUzAwWXpNMExXRXpaVEF0TURjM01HRTNORFEzTXpSbSJ9";
+        Restart = "always";
+        User = "cloudflared";
+        Group = "cloudflared";
+      };
     };
   };
 }
