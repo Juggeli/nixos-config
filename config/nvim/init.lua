d8a0332e42a39a52ad473bd40014eb7bff0be9b0
Jukka Alavesa <juggeli@gmail.com>
2022-12-15 13:51:07 +0200

    update vim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -440,19 +440,19 @@ db.custom_header = {
 db.custom_center = {
 	{
 		icon = "  ",
-		desc = "Recently opened files                   ",
+		desc = "Recently opened files                  ",
 		action = "Telescope oldfiles",
 		shortcut = "SPC f o",
 	},
 	{
 		icon = "  ",
-		desc = "Find  File                              ",
+		desc = "Find File                              ",
 		action = "Telescope find_files find_command=rg,--hidden,--files",
 		shortcut = "SPC f f",
 	},
 	{
 		icon = "  ",
-		desc = "Find  word                              ",
+		desc = "Find word                              ",
 		action = "Telescope live_grep",
 		shortcut = "SPC f w",
 	},
