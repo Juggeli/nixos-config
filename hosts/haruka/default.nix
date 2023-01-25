66eb8643e3864d473451f316d38ed51e859304db
Jukka Alavesa <juggeli@gmail.com>
2023-01-25 17:47:09 +0200

    change ip and enable qbittorrent

--- hosts/haruka/default.nix
+++ hosts/haruka/default.nix
@@ -28,7 +28,7 @@
       grafana.enable = false;
       prometheus.enable = false;
       plex.enable = false;
-      qbittorrent.enable = false;
+      qbittorrent.enable = true;
       jackett.enable = false;
       sonarr.enable = false;
       homeassistant.enable = false;
@@ -45,13 +45,13 @@
   };
 
   networking.interfaces.enp3s0.ipv4.addresses = [{
-    address = "10.11.11.3";
+    address = "10.11.11.2";
     prefixLength = 24;
   }];
   networking.defaultGateway = "10.11.11.1";
   networking.nameservers = [ "1.1.1.1" ];
 
-  boot.kernelParams = [ "ip=10.11.11.3::10.11.11.1:255.255.255.0:haruka:enp3s0:off" ];
+  boot.kernelParams = [ "ip=10.11.11.2::10.11.11.1:255.255.255.0:haruka:enp3s0:off" ];
 
   boot.loader.supportsInitrdSecrets = true;
   boot.initrd = {
