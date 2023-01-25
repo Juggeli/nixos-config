de7008f39d13426760673360c2cd38de99c196a0
Jukka Alavesa <juggeli@gmail.com>
2023-01-25 20:30:39 +0200

    fix ssh keys

--- hosts/asuka/default.nix
+++ hosts/asuka/default.nix
@@ -62,10 +62,10 @@
       enable = true;
       port = 22;
       authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO juggeli@gmail.com" ];
-      hostKeys = [ "hostkey" ];
+      hostKeys = [ /etc/ssh/ssh_host_ed25519_key ];
     };
     secrets = {
-      "/hostkey" = "/etc/ssh/ssh_host_ed25519_key";
+      "/etc/ssh/ssh_host_ed25519_key" = /etc/ssh/ssh_host_ed25519_key;
     };
     network.postCommands = ''
       echo 'cryptsetup-askpass' >> /root/.profile
