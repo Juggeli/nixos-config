b53a4d91b16db80caca5bf744111fa3e0aaf9261
Jukka Alavesa <juggeli@gmail.com>
2023-02-09 22:10:52 +0200

    add autologin script

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -51,7 +51,16 @@
 
   boot.loader.systemd-boot.memtest86.enable = true;
 
-  environment.systemPackages = [ pkgs.cifs-utils ];
+  environment.systemPackages = [ 
+    pkgs.cifs-utils 
+    (pkgs.writeShellScriptBin "lock" ''
+      if [[ "$1" == this ]]
+        then args="-s"
+        else args="-san"
+      fi
+      USER=juggeli ${pkgs.vlock}/bin/vlock "$args"
+    '')
+  ];
   fileSystems."/mnt/pool" = {
     device = "//10.11.11.2/pool";
     fsType = "cifs";
@@ -73,4 +82,24 @@
   # here. Per-interface useDHCP will be mandatory in the future, so this
   # generated config replicates the default behaviour.
   networking.useDHCP = false;
+
+  services.getty.autologinUser = "juggeli";
+
+  hm.programs.fish.loginShellInit = ''
+    if test (tty) = /dev/tty1
+      exec sway
+    else
+      sudo /run/current-system/sw/bin/lock this
+    end
+  '';
+
+  security.sudo = {
+    enable = true;
+    extraConfig = ''
+      juggeli ALL = (root) NOPASSWD: /run/current-system/sw/bin/lock
+      juggeli ALL = (root) NOPASSWD: /run/current-system/sw/bin/lock this
+      juggeli ALL = (root) NOPASSWD: /run/current-system/sw/bin/reboot
+      juggeli ALL = (root) NOPASSWD: /run/current-system/sw/bin/shutdown
+    '';
+  };
 }
