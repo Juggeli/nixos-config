9e620a46bb6ae8f146680e0ead22b105cefd5b3c
Jukka Alavesa <juggeli@gmail.com>
2023-02-18 00:31:38 +0200

    update neovim confs

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -13,57 +13,22 @@ in
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
+      neovim
+      gcc
+      rustc
+      nodejs
       lazygit
       ripgrep
       tree-sitter
-      rnix-lsp
-      sumneko-lua-language-server
-      nodePackages.prettier
       stylua
-      nodePackages.eslint
+      sumneko-lua-language-server
     ];
 
     programs.neovim.defaultEditor = true;
 
-    hm.programs.neovim = {
-      enable = true;
-      vimAlias = true;
-      viAlias = true;
-      vimdiffAlias = true;
-      plugins = with pkgs.vimPlugins; [
-        telescope-nvim
-        telescope-symbols-nvim
-        telescope-fzf-native-nvim
-        nvim-treesitter.withAllGrammars
-        nvim-lspconfig
-        cmp-nvim-lsp
-        nvim-cmp
-        cmp-path
-        cmp-buffer
-        lspsaga-nvim
-        null-ls-nvim
-        gitsigns-nvim
-        nvim-autopairs
-        nvim-ts-autotag
-        comment-nvim
-        lualine-nvim
-        luasnip
-        cmp_luasnip
-        friendly-snippets
-        lspkind-nvim
-        nvim-tree-lua
-        vim-tmux-navigator
-        vim-surround
-        nvim-web-devicons
-        bufferline-nvim
-        nvim-lastplace
-        bufdelete-nvim
-        which-key-nvim
-        vim-hexokinase
-        material-nvim
-        alpha-nvim
-      ];
-      extraConfig = "lua << EOF\n" + builtins.readFile "${configDir}/nvim/init.lua" + "\nEOF";
+    hm.home.file.".config/nvim/" = {
+      source = "${configDir}/nvim/";
+      recursive = true;
     };
   };
 }
