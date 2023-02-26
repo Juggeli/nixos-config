f0ec51475cecb4723c7b39de592034fef6f66aae
Jukka Alavesa <juggeli@gmail.com>
2023-02-26 21:41:48 +0200

    add rust

---
+++ modules/tools/rust/default.nix
@@ -0,0 +1,25 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+with lib.internal;
+let
+  cfg = config.plusultra.tools.rust;
+in
+{
+  options.plusultra.tools.rust = with types; {
+    enable = mkBoolOpt false "Whether or not to install and configure rust env.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      rustup
+      clang
+      pkg-config
+    ];
+
+    plusultra.home.extraOptions = {
+      home.sessionPath = [ "$HOME/.cargo/bin" ];
+    };
+  };
+}
+
