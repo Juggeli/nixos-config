71a971b4dd8b9aefe707ee36c95b01308f932f00
Jukka Alavesa <juggeli@gmail.com>
2023-03-19 03:20:41 +0200

    add secrets

--- systems/x86_64-linux/haruka/secrets/secrets.nix
+++ systems/x86_64-linux/haruka/secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPwDXLTCnNPVKSLHgbzlcgdbb6Ra+L2jZJfOJaSgom9 juggeli@gmail.com";
+  key2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiEGRIqO6CX4uzbHi2Qzja8gX+oxm93AOm8Q62VreSc root@haruka";
 in
 {
-  "borg-passkey.age".publicKeys = [ key ];
+  "borg-passkey.age".publicKeys = [ key key2 ];
 }
 
