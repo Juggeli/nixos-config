b148b7752db1082af80c76fbf66f6fb502541c5b
Jukka Alavesa <juggeli@gmail.com>
2023-04-01 23:47:01 +0300

    fix autologin

--- systems/x86_64-linux/noel/autologin.nix
+++ systems/x86_64-linux/noel/autologin.nix
@@ -1,22 +1,12 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.cifs-utils
-    (pkgs.writeShellScriptBin "lock" ''
-      if [[ "$1" == this ]]
-        then args="-s"
-        else args="-san"
-      fi
-      USER=juggeli ${pkgs.vlock}/bin/vlock "$args"
-    '')
-  ];
   services.getty.autologinUser = "juggeli";
-  programs.fish.loginShellInit = ''
+  plusultra.home.extraOptions.programs.fish.loginShellInit = ''
     if test (tty) = /dev/tty1
       exec sway
     else
-      doas /run/current-system/sw/bin/lock this
+      exec ${pkgs.vlock}/bin/vlock
     end
   '';
 }
