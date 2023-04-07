12404cb636b735136f2377d96fe9b295901c455e
Jukka Alavesa <juggeli@gmail.com>
2023-04-08 02:02:22 +0300

    rm qbittorrent themes

--- modules/services/qbittorrent/default.nix
+++ modules/services/qbittorrent/default.nix
@@ -11,24 +11,26 @@ in
 
   config = mkIf cfg.enable {
     virtualisation.oci-containers.containers.qbittorrent = {
-      image = "dyonr/qbittorrentvpn:latest";
+      image = "cr.hotio.dev/hotio/qbittorrent";
       autoStart = true;
       ports = [ "8080:8080" ];
       volumes = [
         "/mnt/appdata/qbittorrent:/config"
         "/mnt/pool/downloads/:/mnt/pool/downloads/"
         "/mnt/pool/media/:/mnt/pool/media/"
-        "${pkgs.plusultra.vuetorrent}/:/vuetorrent/"
       ];
       environment = {
-        VPN_ENABLED = "yes";
-        VPN_TYPE = "wireguard";
-        LAN_NETWORK = "10.11.11.0/24,172.20.0.0/16";
+        VPN_ENABLED = "true";
+        VPN_LAN_NETWORK = "10.11.11.0/24,172.20.0.0/16";
+        VPN_CONF = "wg0";
+        PRIVOXY_ENABLED = "false";
         PUID = "1000";
         PGID = "100";
       };
       extraOptions = [
         "--cap-add=NET_ADMIN"
+        ''--sysctl="net.ipv4.conf.all.src_valid_mark=1"''
+        ''--sysctl="net.ipv6.conf.all.disable_ipv6=1"''
       ];
     };
   };
--- packages/vuetorrent/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, lib, ... }:
-
-pkgs.fetchzip {
-  url = "https://github.com/WDaan/VueTorrent/releases/download/v1.5.3/vuetorrent.zip";
-  sha256 = "sha256-sq7OlFYKh54NXOFoF+zWmSZLf26978OOEH1+qRcU6aw=";
-  name = "vuetorrent";
-}
