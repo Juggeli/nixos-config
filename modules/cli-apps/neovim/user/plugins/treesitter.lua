8f9f9095cff34ed2db08b6052b7127f703f7eddc
Jukka Alavesa <juggeli@gmail.com>
2023-04-07 22:43:04 +0300

    fix nvim again

--- modules/cli-apps/neovim/user/plugins/treesitter.lua
+++ modules/cli-apps/neovim/user/plugins/treesitter.lua
@@ -1,9 +1,8 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	config = function()
+	opts = function(_, opts)
+		opts.ensure_installed = { "lua", "nix" }
 		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "nix" },
-		})
+		return opts
 	end,
 }
