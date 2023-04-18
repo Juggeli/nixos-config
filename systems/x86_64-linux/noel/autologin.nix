a02749fee82cd89d14f4a84f6f54627e7c3470b2
Jukka Alavesa <juggeli@gmail.com>
2023-04-18 19:58:08 +0300

    disable vlock

--- systems/x86_64-linux/noel/autologin.nix
+++ systems/x86_64-linux/noel/autologin.nix
@@ -5,8 +5,8 @@
   plusultra.home.extraOptions.programs.fish.loginShellInit = ''
     if test (tty) = /dev/tty1
       exec sway
-    else
-      exec ${pkgs.vlock}/bin/vlock
+    # else
+    #   exec ${pkgs.vlock}/bin/vlock
     end
   '';
 }
