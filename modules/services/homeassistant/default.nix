834b251e804007a786a6e3225b3e0f70ba5d87f2
Jukka Alavesa <juggeli@gmail.com>
2023-07-29 19:49:40 +0300

    home assistant updates

--- modules/services/homeassistant/default.nix
+++ modules/services/homeassistant/default.nix
@@ -2,27 +2,27 @@
 
 with lib;
 with lib.internal;
-let 
+let
   cfg = config.plusultra.services.homeassistant;
-  mkLovelaceModule = name: {
-    url = "/local/${name}.js?${pkgs.nur.repos.mweinelt.hassLovelaceModules."${name}".version}";
-    type = "module";
-  };
 in
 {
   options.plusultra.services.homeassistant = with types; {
     enable = mkBoolOpt false "Whether or not to enable homeassistant service.";
   };
 
-  config = mkIf cfg.enable { 
+  config = mkIf cfg.enable {
     nixpkgs.overlays = [ inputs.nur.overlay ];
 
     services.home-assistant = {
       enable = true;
+      configDir = "/mnt/appdata/home-assistant";
       package = (pkgs.home-assistant.override {
         extraPackages = py: with py; [ psycopg2 ];
-      }).overrideAttrs (oldAttrs: {
+      }).overrideAttrs (old: {
         doInstallCheck = false;
+        patches = (old.patches or [ ]) ++ [
+          ./static-symlinks.patch
+        ];
       });
       config.recorder.db_url = "postgresql://@/hass";
       extraComponents = [
@@ -45,51 +45,45 @@ in
         "zha"
         "homekit"
         "ipp"
+        "broadlink"
+        "thread"
+        "google_translate"
       ];
       config = {
         default_config = { };
+        homeassistant = {
+          name = "Home";
+          latitude = "!secret latitude";
+          longitude = "!secret longitude";
+          elevation = "!secret elevation";
+          unit_system = "metric";
+          temperature_unit = "C";
+          time_zone = "Europe/Helsinki";
+        };
         http = {
-          server_host = "::1";
-          trusted_proxies = [ "::1" ];
+          server_host = "10.11.11.2";
+          trusted_proxies = [ "10.11.11.2" ];
           use_x_forwarded_for = true;
         };
+        mqtt = { };
+        yeelight = { };
       };
     };
 
     systemd.tmpfiles.rules = [
-      "d /run/hass 0700 nginx nginx"
-      "L+ /run/hass/apexcharts-card.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.apexcharts-card}/apexcharts-card.js"
-      "L+ /run/hass/mushroom.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.mushroom}/mushroom.js"
+      "d /var/lib/hass/www 0755 hass hass"
+      "L /var/lib/hass/www/apexcharts-card.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.apexcharts-card}/apexcharts-card.js"
+      "L /var/lib/hass/www/mushroom.js - - - - ${pkgs.nur.repos.mweinelt.hassLovelaceModules.mushroom}/mushroom.js"
     ];
 
-    services.home-assistant.config.lovelace = {
-      resources = [
-        (mkLovelaceModule "apexcharts-card")
-        (mkLovelaceModule "mushroom")
-      ];
-    };
-
-    services.nginx = {
-      enable = true;
-      recommendedProxySettings = true;
-      virtualHosts."hass.jugi.cc" = {
-        forceSSL = true;
-        enableACME = true;
-        extraConfig = ''
-          proxy_buffering off;
-        '';
-        locations."/" = {
-          proxyPass = "http://[::1]:8123";
-          proxyWebsockets = true;
-        };
-        locations."/local/" = {
-          alias = "/run/hass/";
-        };
-      };
-    };
+    services.home-assistant.config.lovelace.resources = [
+      { url = "local/apexcharts-card.js"; type = "module"; }
+      { url = "local/mushroom.js"; type = "module"; }
+    ];
 
     services.postgresql = {
       enable = true;
+      dataDir = "/mnt/appdata/postgresql";
       ensureDatabases = [ "hass" ];
       ensureUsers = [{
         name = "hass";
@@ -109,10 +103,13 @@ in
     };
 
     services.zigbee2mqtt = {
-      enable = false;
-      dataDir = "/mnt/pool/appdata/zigbee2mqtt";
+      enable = true;
+      dataDir = "/mnt/appdata/zigbee2mqtt";
       settings = {
-        homeassistant = config.services.home-assistant.enable;
+        homeassistant = true;
+        mqtt = {
+          server = "mqtt://localhost:1883";
+        };
         serial = {
           port = "/dev/ttyUSB0";
         };
@@ -120,11 +117,17 @@ in
           port = 8090;
           host = "10.11.11.2";
         };
+        advanced = {
+          channel = 25;
+          pan_id = 6754;
+        };
+        groups = "groups.yaml";
       };
     };
 
     networking.firewall = {
-      allowedTCPPorts = [ 80 443 config.services.home-assistant.config.http.server_port 8090 ];
+      allowedTCPPorts = [ 8123 8090 1400 ];
+      allowedUDPPorts = [ 5353 ];
     };
   };
 }
