4df360728c5a0ff01865dc2a749cafb1da23adec
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-01-25 00:12:18 +0200

    set kernel to 6.6

--- systems/x86_64-linux/noel/hardware.nix
+++ systems/x86_64-linux/noel/hardware.nix
@@ -13,7 +13,7 @@ in
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_6;
 
     initrd = {
       availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "aesni_intel" ];
