c564e4da60b2e41a9511dfa4431a493241c0d4d6
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-01-26 20:34:24 +0200

    fix xdg portals

--- modules/nixos/desktop/addons/xdg-portal/default.nix
+++ modules/nixos/desktop/addons/xdg-portal/default.nix
@@ -12,6 +12,10 @@ in
     xdg.portal = {
       enable = true;
       wlr.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-hyprland
+      ];
     };
 
     xdg.mime.enable = true;
