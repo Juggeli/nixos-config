82e843d85be27b15ea8a8f43b655d77c71019b3d
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-01-27 18:41:59 +0200

    add hydrus desktop item

--- modules/home/apps/hydrus/default.nix
+++ modules/home/apps/hydrus/default.nix
@@ -2,6 +2,11 @@
 with lib;
 with lib.plusultra; let
   cfg = config.plusultra.apps.hydrus;
+  hydrusDesktopItem = pkgs.makeDesktopItem {
+    name = "hydrus-client";
+    desktopName = "Hydrus Client";
+    exec = "${pkgs.hydrus}/bin/hydrus-client";
+  };
 in
 {
   options.plusultra.apps.hydrus = with types; {
@@ -11,6 +16,7 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       hydrus
+      hydrusDesktopItem
     ];
   };
 }
