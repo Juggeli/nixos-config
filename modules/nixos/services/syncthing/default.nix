cbba2efb5dd2ccbbacc8d17c135ddee6c356edce
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-02-13 20:00:11 +0200

    remove iphone from syncthing

--- modules/nixos/services/syncthing/default.nix
+++ modules/nixos/services/syncthing/default.nix
@@ -24,7 +24,6 @@ in
             "air" = { id = "TDYWB6T-LY5VAOF-X25VOXH-H6L3RUU-CN3ZFDM-NCM7YDY-NAI2P6H-723IBQZ"; };
             "haruka" = { id = "45RLMRL-COTJJV7-QXRIMZC-E2UR3P5-X5DV62Q-X6EO5HY-I4RJISU-BTPXIAB"; };
             "noel" = { id = "7WH7YG3-7UCT4KC-R27XT6G-RC6C7OF-JFQJJEH-JNVDCZJ-ZUZFFK4-3O25GQT"; };
-            "iphone" = { id = "TO3YS4C-R4DPILJ-K7HNKRA-HUCB7MX-X7O5DR4-CP7OHOD-RRCX5BN-7PWIMQE"; };
           };
           folders = {
             "documents" = {
