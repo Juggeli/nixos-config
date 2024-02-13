95759b50315605a01f004c810f7a80339dac16e7
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-02-13 23:17:39 +0200

    put grub back in

--- systems/x86_64-linux/brr/default.nix
+++ systems/x86_64-linux/brr/default.nix
@@ -12,14 +12,15 @@ with lib.plusultra; {
     suites = {
       common-slim = enabled;
     };
-
-    services = {
-      openssh = {
-        enable = true;
-      };
-    };
+    system.boot = lib.mkForce disabled;
   };
 
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
   boot.loader.supportsInitrdSecrets = true;
   boot.initrd = {
     luks.forceLuksSupportInInitrd = true;
