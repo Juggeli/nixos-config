4913d050e5833d2de083727c4d3741b54a2d4dfb
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-02-16 15:08:58 +0200

    add javrter

--- modules/home/cli-apps/fish/default.nix
+++ modules/home/cli-apps/fish/default.nix
@@ -18,6 +18,7 @@ in
       lazygit
       fzf
       du-dust
+      gum
     ];
     programs.fish = {
       enable = true;
---
+++ modules/nixos/user/fish/javrter.fish
@@ -0,0 +1,40 @@
+function javrter
+    set -g base_dir /mnt/pool/sorted/jav/sorted
+
+    set -g dirs (fd -t d -d 1 . $base_dir | shuf)
+    set -g delete_dirs
+
+    for dir in $dirs
+        echo "Checking $dir"
+        if not test -f $dir/.keep
+            fd -e mkv -e mp4 . "$dir" | mpv --really-quiet --playlist=-
+            set choice ( gum choose "keep" "delete" "quit" )
+            switch $choice
+                case keep
+                    echo "Keeping $dir"
+                    touch $dir/.keep
+                case delete
+                    echo "Deleting $dir"
+                    set delete_dirs $delete_dirs $dir
+                case quit
+                    echo "Quitting..."
+                    break
+            end
+        end
+    end
+
+
+    if test -n "$delete_dirs"
+        echo "Files to be deleted:"
+        for dir in $delete_dirs
+            echo $dir
+        end
+        if gum confirm "Delete files?"
+            echo "Deleting files..."
+            for dir in $delete_dirs
+                echo "Deleting $dir"
+                rm -rf "$dir"
+            end
+        end
+    end
+end
