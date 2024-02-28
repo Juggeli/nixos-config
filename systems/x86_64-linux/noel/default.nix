a1b1173a7ec8bd5e327891cc5dcc2b13047bff65
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-02-28 21:04:13 +0200

    update secrets

--- systems/x86_64-linux/noel/default.nix
+++ systems/x86_64-linux/noel/default.nix
@@ -56,7 +56,7 @@ with lib.plusultra; {
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1001,gid=100" ];
+      [ "${automount_opts},credentials=${config.age.secrets.smb.path},uid=1000,gid=100" ];
   };
 
   # systemd.extraConfig = ''
--- systems/x86_64-linux/noel/secrets/secrets.nix
+++ systems/x86_64-linux/noel/secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO juggeli@gmail.com";
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqG5+CvQJOb1Jjb9UY7rOVj6Ij8MfThYcWpnBw9G9YH";
 in
 {
-  "borg-passkey.age".publicKeys = [ key ];
+  "borg-passkey.age".publicKeys = [ user system ];
+  "smb.age".publicKeys = [ user system ];
 }
