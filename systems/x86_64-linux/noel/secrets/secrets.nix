c0ccf4fd5128dbf13436c8215f1708ac2ba46b15
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-02-29 16:43:23 +0200

    rekey secrets again

--- systems/x86_64-linux/noel/secrets/secrets.nix
+++ systems/x86_64-linux/noel/secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO";
-  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqG5+CvQJOb1Jjb9UY7rOVj6Ij8MfThYcWpnBw9G9YH";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRcPjReo8vFNgTRYYaJ6Q+wYdOxF414AFJuF3utHyd2";
 in
 {
   "borg-passkey.age".publicKeys = [ user system ];
