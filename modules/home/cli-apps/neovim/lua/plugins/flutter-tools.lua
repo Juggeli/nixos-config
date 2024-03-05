41c1f18684b675fd9fbe8e4ebeb7ddb6d944944e
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-03-05 15:36:33 +0200

    update nvim settings

--- modules/home/cli-apps/neovim/lua/plugins/flutter-tools.lua
+++ modules/home/cli-apps/neovim/lua/plugins/flutter-tools.lua
@@ -12,6 +12,8 @@ return {
 			{ "<leader>rR", "<cmd>FlutterRestart<cr>", desc = "Restart flutter app" },
 			{ "<leader>rh", "<cmd>FlutterReload<cr>", desc = "Reload flutter app" },
 			{ "<leader>rd", "<cmd>FlutterDevices<cr>", desc = "Run flutter app in selected device" },
+			{ "<leader>rq", "<cmd>FlutterQuit<cr>", desc = "Quit running flutter app" },
+			{ "<leader>re", "<cmd>FlutterEmulators<cr>", desc = "Run flutter app in selected emulator" },
 		},
 	},
 }
