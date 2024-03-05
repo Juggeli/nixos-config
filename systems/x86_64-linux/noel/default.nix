ec39cd05ead3f2e3a120485a7091dfdb3cd9fb08
Jukka Alavesa <juggeli@gmail.com>
2024-03-05 18:14:51 +0200

    disable graphical boot

--- systems/x86_64-linux/noel/default.nix
+++ systems/x86_64-linux/noel/default.nix
@@ -10,7 +10,7 @@ with lib.plusultra; {
   plusultra = {
     feature = {
       boot = enabled;
-      graphical = enabled;
+      graphical = disabled;
       syncthing = enabled;
       borgmatic = {
         enable = true;
