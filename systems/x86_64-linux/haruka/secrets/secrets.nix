771de0410064be56bcc3e17e42d72cda6015a2eb
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-03-07 15:41:03 +0200

    update haruka secrets

--- systems/x86_64-linux/haruka/secrets/secrets.nix
+++ systems/x86_64-linux/haruka/secrets/secrets.nix
@@ -1,9 +1,14 @@
 let
-  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO juggeli@gmail.com";
-  key2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiEGRIqO6CX4uzbHi2Qzja8gX+oxm93AOm8Q62VreSc root@haruka";
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPwDXLTCnNPVKSLHgbzlcgdbb6Ra+L2jZJfOJaSgom9";
+  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO";
+  system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiEGRIqO6CX4uzbHi2Qzja8gX+oxm93AOm8Q62VreSc";
 in
 {
-  "borg-passkey.age".publicKeys = [ key key2 ];
-  "cloudflared.age".publicKeys = [ key key2 ];
+  "borg-passkey.age".publicKeys = [ user user2 system ];
+  "cloudflared.age".publicKeys = [ user user2 system ];
+  "syncthing-key.age".publicKeys = [ user user2 system ];
+  "syncthing-cert.age".publicKeys = [ user user2 system ];
+  "borg-healthcheck.age".publicKeys = [ user user2 system ];
+  "storagebox-url.age".publicKeys = [ user user2 system ];
 }
 
