8d740111a8ced89f6f689e0eb5a3709dfe046856
Jukka Alavesa <jukka.alavesa@codemate.com>
2024-03-07 15:40:02 +0200

    update tailscale confs

--- modules/nixos/services/tailscale/default.nix
+++ modules/nixos/services/tailscale/default.nix
@@ -26,7 +26,7 @@ in
     services.tailscale = {
       enable = true;
       port = cfg.port;
-      authKeyFile = cfg.autoconnect.key;
+      authKeyFile = mkIf cfg.autoconnect.enable cfg.autoconnect.key;
     };
 
     plusultra.filesystem.impermanence.directories = [
@@ -49,30 +49,5 @@ in
 
       networkmanager.unmanaged = [ "tailscale0" ];
     };
-
-    # systemd.services.tailscale-autoconnect = mkIf cfg.autoconnect.enable {
-    #   description = "Automatic connection to Tailscale";
-    #
-    #   # Make sure tailscale is running before trying to connect to tailscale
-    #   after = [ "network-pre.target" "tailscale.service" ];
-    #   wants = [ "network-pre.target" "tailscale.service" ];
-    #   wantedBy = [ "multi-user.target" ];
-    #
-    #   # Set this service as a oneshot job
-    #   serviceConfig.Type = "oneshot";
-    #
-    #   # Have the job run this shell script
-    #   script = with pkgs; ''
-    #     # Wait for tailscaled to settle
-    #     sleep 2
-    #     # Check if we are already authenticated to tailscale
-    #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-    #     if [ $status = "Running" ]; then # if so, then do nothing
-    #       exit 0
-    #     fi
-    #     # Otherwise authenticate with tailscale
-    #     ${tailscale}/bin/tailscale up -authkey "${cfg.autoconnect.key}"
-    #   '';
-    # };
   };
 }
