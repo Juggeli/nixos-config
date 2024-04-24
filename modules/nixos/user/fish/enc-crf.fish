5297a09f25b9ff68ce92bbbdcb91122be6ce64d8
Jukka Alavesa <juggeli@gmail.com>
2024-04-24 19:18:14 +0300

    update enc-crf

--- modules/nixos/user/fish/enc-crf.fish
+++ modules/nixos/user/fish/enc-crf.fish
@@ -1,9 +1,9 @@
 function enc-crf
-  ab-av1 encode -e libsvtav1 --preset 10 \
-    --acodec libopus --enc b:a=48k --enc vbr=on \
-    --enc compression_level=10 --enc frame_duration=60 \
-    --enc application=audio --crf 30 \
-    --pix-format yuv420p10le \
-    -o $(path change-extension '' $argv).av1.mkv \
-    -i $argv
+    ab-av1 encode -e libsvtav1 --preset 8 \
+        --acodec libopus --enc b:a=48k --enc vbr=on \
+        --enc compression_level=10 --enc frame_duration=60 \
+        --enc application=audio --crf 30 \
+        --pix-format yuv420p10le \
+        -o $(path change-extension '' $argv).av1.mkv \
+        -i $argv
 end
