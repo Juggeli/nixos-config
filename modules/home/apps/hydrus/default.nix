dda0dd64a5ed3399dbad0252aba4cdef6a0fa5d0
Jukka Alavesa <juggeli@gmail.com>
2024-07-11 22:14:21 +0300

    remove hydrus desktop item

--- modules/home/apps/hydrus/default.nix
+++ modules/home/apps/hydrus/default.nix
@@ -2,11 +2,6 @@
 with lib;
 with lib.plusultra; let
   cfg = config.plusultra.apps.hydrus;
-  hydrusDesktopItem = pkgs.makeDesktopItem {
-    name = "hydrus-client";
-    desktopName = "Hydrus Client";
-    exec = "${pkgs.hydrus}/bin/hydrus-client";
-  };
 in
 {
   options.plusultra.apps.hydrus = with types; {
@@ -16,7 +11,6 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       hydrus
-      hydrusDesktopItem
     ];
   };
 }
