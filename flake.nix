5ba11b0367ef2775bd714966381f6c2b560135c4
Jukka Alavesa <juggeli@gmail.com>
2025-05-20 20:22:40 +0300

    remove unnecessary permittedInsecurePackages

--- flake.nix
+++ flake.nix
@@ -73,10 +73,6 @@
     lib.mkFlake {
       channels-config = {
         allowUnfree = true;
-        permittedInsecurePackages = [
-          "electron-25.9.0"
-          "freeimage-unstable-2021-11-01"
-        ];
       };
 
       overlays = with inputs; [
