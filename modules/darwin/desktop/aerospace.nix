fd5ac4d0489a40e7eaaacb3f62896c3980091041
Jukka Alavesa <jukka.alavesa@codemate.com>
2025-06-11 13:26:24 +0300

    update aerospace configs

--- modules/darwin/desktop/aerospace.nix
+++ modules/darwin/desktop/aerospace.nix
@@ -13,39 +13,39 @@ let
     focused_window_id=$(aerospace list-windows --focused --format "%{app-bundle-id}")
 
     switch_to_us() {
-        keyboardSwitcher select 'U.S.'
+      keyboardSwitcher select 'U.S.'
     }
 
     switch_to_finnish() {
-        keyboardSwitcher select 'Finnish'
+      keyboardSwitcher select 'Finnish'
     }
 
     case "$focused_window_id" in
-        "com.tinyspeck.slackmacgap")
-            switch_to_finnish
-            ;;
-        "com.google.Chrome")
-            switch_to_finnish
-            ;;
-        "org.mozilla.firefox")
-            switch_to_finnish
-            ;;
-        "com.apple.mail")
-            switch_to_finnish
-            ;;
-        "com.hnc.Discord")
-            switch_to_finnish                              
-            ;;
-        "org.mozilla.com.zen.browser")
-            switch_to_finnish
-            ;;
-        "md.obsidian")
-            switch_to_finnish
-            ;;
-        *)
-            # Default layout if the app is not recognized
-            switch_to_us
-            ;;
+    "com.tinyspeck.slackmacgap")
+      switch_to_finnish
+      ;;
+    "com.google.Chrome")
+      switch_to_finnish
+      ;;
+    "org.mozilla.firefox")
+      switch_to_finnish
+      ;;
+    "com.apple.mail")
+      switch_to_finnish
+      ;;
+    "com.hnc.Discord")
+      switch_to_finnish
+      ;;
+    "app.zen-browser.zen")
+      switch_to_finnish
+      ;;
+    "md.obsidian")
+      switch_to_finnish
+      ;;
+    *)
+      # Default layout if the app is not recognized
+      switch_to_us
+      ;;
     esac
   '';
 in
