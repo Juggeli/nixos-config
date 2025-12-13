c850c3a6f835b2b1fe58cad53c0bc8fc5da1d971
Jukka Alavesa <juggeli@gmail.com>
2025-12-13 14:38:45 +0200

    🔥 fix: remove unused plugins from claude-code settings

--- modules/home/cli-apps/claude-code.nix
+++ modules/home/cli-apps/claude-code.nix
@@ -34,8 +34,6 @@ in
       home.file.".claude/settings.json".text = builtins.toJSON {
         enabledPlugins = {
           "superpowers@superpowers-marketplace" = true;
-          # "claude-opus-4-5-migration@claude-code-plugins"= true;
-          "frontend-design@claude-code-plugins" = true;
         };
         alwaysThinkingEnabled = true;
         cleanupPeriodDays = 99999;
