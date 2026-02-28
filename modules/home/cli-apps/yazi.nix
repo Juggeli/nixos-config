ee2a38ef9988a11a98eccb4f2fc9ec11e6b647cb
Jukka Alavesa <juggeli@gmail.com>
2026-02-28 21:07:42 +0200

    ✨ feat: add archives.yazi plugin with compression tools

--- modules/home/cli-apps/yazi.nix
+++ modules/home/cli-apps/yazi.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -21,6 +22,43 @@ in
         preview = {
           image_delay = 0;
         };
+      };
+      extraPackages = with pkgs; [
+        p7zip
+        pigz
+        pbzip2
+        pixz
+        lz4
+        zstd
+        unrar
+      ];
+      plugins = {
+        archives = pkgs.fetchFromGitHub {
+          owner = "maximtrp";
+          repo = "archives.yazi";
+          rev = "fa8740f8c9360c569693a029c22d309e64d1f3a6";
+          hash = "sha256-dW53xjfa6FUyqh+SOSPHBMOm1SqSyBwZGG9zE1aldg4=";
+        };
+      };
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            on = [
+              "c"
+              "z"
+            ];
+            run = "plugin archives -- compress";
+            desc = "Compress selection";
+          }
+          {
+            on = [
+              "c"
+              "x"
+            ];
+            run = "plugin archives -- extract";
+            desc = "Extract archive";
+          }
+        ];
       };
     };
     catppuccin.yazi.enable = true;
