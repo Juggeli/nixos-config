e928a47dfae497137fb06fc4ba21076665fda94d
Jukka Alavesa <jukka.alavesa@codemate.com>
2026-08-04 14:57:37 +0300

    feat(agenix): add kuro host key to shared secrets

--- modules/common/agenix-shared/_secrets/secrets.nix
+++ modules/common/agenix-shared/_secrets/secrets.nix
@@ -2,12 +2,14 @@ let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPwDXLTCnNPVKSLHgbzlcgdbb6Ra+L2jZJfOJaSgom9";
   user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpvXZ6hWXrKgvX1ce+v+tmjYO2EuW9YjS8o5N7vmfRO";
   monday_chan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbJeg8M8Pmbab+/X5on+hFEJlLW0/f4vX8nNtDNAcox";
+  kuro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9WCTLyOQMqPuWuBrqHDO+MbTHRrdeGcXAkjl1pj0Z3";
   noel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRcPjReo8vFNgTRYYaJ6Q+wYdOxF414AFJuF3utHyd2";
   haruka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiEGRIqO6CX4uzbHi2Qzja8gX+oxm93AOm8Q62VreSc";
   keys = [
     user1
     user2
     monday_chan
+    kuro
     noel
     haruka
   ];
